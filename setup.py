b2bacbf8ca3212e8c4e51b4165bdaff76f392193
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2020-07-01 17:32:42 +0100

    adding setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="roboclimate",
+    version="0.0.1",
+    author="falvarezb",
+    author_email="falvarez.berron@gmail.com",
+    description="Evaluation of climate models accuracy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/falvarezb/roboclimate",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+)
