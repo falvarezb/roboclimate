26d16487139a53cf35e6f5369f2eb525d46901b7
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-01-22 00:13:23 +0000

    refactoring

--- roboclimate/data_quality.py
+++ roboclimate/data_quality.py
@@ -21,7 +21,7 @@ def load_csv_files(city: City) -> dict:
     return {"true_temp_df": true_temp_df, "forecast_temp_df": forecast_temp_df, "join_data_df": join_data_df, "metrics_df": metrics_df}
 
 
-def dts(start, end=dt.datetime.now()):
+def dts(start: dt.datetime, end: dt.datetime = dt.datetime.now()):
     """
     List of date-times for which a temperature should be recorded
     In theory, the times 0,3,6,9,12,15,18,21 of each day
@@ -29,46 +29,50 @@ def dts(start, end=dt.datetime.now()):
     return pd.DataFrame(data=rutil.date_and_timestamp(start, end), columns=['dt', 'today'])
 
 
-def missing_temps(city: City) -> pd.DataFrame:
+def missing_temps(city: City, start_dt: dt.datetime = None, end_dt: dt.datetime = dt.datetime.now()) -> pd.DataFrame:
     """
     Finds dts for which the temperature was not recorded
     Returns the right join between the DataFrames: 'true_temp_df' and 'dts'
     """
-    df = load_csv_files(city)
-    merged = df['true_temp_df'].merge(dts(city.firstMeasurement), how='right', on='dt', indicator=True)
+    start_dt = start_dt if start_dt else city.firstMeasurement
+    files = load_csv_files(city)
+    merged = files['true_temp_df'].merge(dts(start_dt, end_dt), how='right', on='dt', indicator=True)
     return merged[merged['_merge'] == 'right_only']  # .groupby('today_y').count()['_merge']
 
 
-def unexpected_temps(city) -> pd.DataFrame:
+def unexpected_temps(city: City, start_dt: dt.datetime = None, end_dt: dt.datetime = dt.datetime.now()) -> pd.DataFrame:
     """
     Finds temperatures recorded at dts other than the times 0,3,6,9,12,15,18,21 of each day
     Returns the left join between the DataFrames: 'true_temp_df' and 'dts'
     """
-    df = load_csv_files(city)
-    merged = df['true_temp_df'].merge(dts(city.firstMeasurement), how='left', on='dt', indicator=True)
+    start_dt = start_dt if start_dt else city.firstMeasurement
+    files = load_csv_files(city)
+    merged = files['true_temp_df'].merge(dts(start_dt, end_dt), how='left', on='dt', indicator=True)
     return merged[merged['_merge'] == 'left_only']  # .groupby('today_x').count()['_merge']
 
 
-def temps_without_five_forecasts(city) -> pd.DataFrame:
+def temps_without_five_forecasts(city: City, start_dt: dt.datetime = None, end_dt: dt.datetime = dt.datetime.now()) -> pd.DataFrame:
     """
     Finds dts for which no all five forecasts were made
 
     Given that dts without the five forecasts are omitted when constructing 'join_data_df',
     said dts can be found by doing the right join between the DataFrames: 'join_data_df' and 'dts'
     """
-    df = load_csv_files(city)
-    merged = df['join_data_df'].merge(dts(city.firstMeasurement), how='right', on='dt', indicator=True)
+    start_dt = start_dt if start_dt else city.firstMeasurement
+    files = load_csv_files(city)
+    merged = files['join_data_df'].merge(dts(start_dt, end_dt), how='right', on='dt', indicator=True)
     return merged[merged['_merge'] == 'right_only']  # .groupby('today_y').count()['_merge']
 
 
-def missing_forecasts(city) -> pd.DataFrame:
+def missing_forecasts(city: City, start_dt: dt.datetime = None, end_dt: dt.datetime = dt.datetime.now()) -> pd.DataFrame:
     """
     Finds days when no forecast was made
     Returns the right join between the DataFrames: 'forecast_temp_df' and 'dts'
     """
-    df = load_csv_files(city)
-    merged = df['forecast_temp_df'].merge(dts(city.firstMeasurement), how='right', on='today', indicator=True)
-    return merged[merged['_merge'] == 'right_only']  # .groupby('today_x').count()['_merge']
+    start_dt = start_dt if start_dt else city.firstMeasurement
+    files = load_csv_files(city)
+    merged = files['forecast_temp_df'].merge(dts(start_dt, end_dt), how='right', on='today', indicator=True)
+    return merged[merged['_merge'] == 'right_only'].groupby('today').count().index.values
 
 
 if __name__ == "__main__":
--- tests/data_quality_test.py
+++ tests/data_quality_test.py
@@ -1,4 +1,5 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, ANY
+import datetime as dt
 import pandas as pd
 import roboclimate.data_quality as rdq
 
@@ -13,6 +14,31 @@ def test_missing_temps(dts_mock, load_csv_files_mock):
     result = rdq.missing_temps(city)
     assert len(result) == 1
     assert result.iloc[0]['dt'] == 400
+
+
+@patch('roboclimate.data_quality.load_csv_files')
+@patch('roboclimate.data_quality.dts')
+def test_missing_temps_without_dates(dts_mock, load_csv_files_mock):
+    load_csv_files_mock.return_value = {"true_temp_df": pd.DataFrame({'temp': [1, 2, 3], 'dt': [100, 200, 300]})}
+    dts_mock.return_value = pd.DataFrame({'dt': [100, 200, 300, 400]})
+    start_dt = dt.datetime(2020, 11, 28, 3, 0, 0, tzinfo=dt.timezone.utc)
+    city = rdq.City('london', start_dt)
+
+    rdq.missing_temps(city)
+    dts_mock.assert_called_once_with(start_dt, ANY)
+
+
+@patch('roboclimate.data_quality.load_csv_files')
+@patch('roboclimate.data_quality.dts')
+def test_missing_temps_with_dates(dts_mock, load_csv_files_mock):
+    load_csv_files_mock.return_value = {"true_temp_df": pd.DataFrame({'temp': [1, 2, 3], 'dt': [100, 200, 300]})}
+    dts_mock.return_value = pd.DataFrame({'dt': [100, 200, 300, 400]})
+    start_dt = dt.datetime(2020, 11, 28, 3, 0, 0, tzinfo=dt.timezone.utc)
+    end_dt = dt.datetime(2020, 11, 28, 4, 0, 0, tzinfo=dt.timezone.utc)
+    city = Mock()
+
+    rdq.missing_temps(city, start_dt, end_dt)
+    dts_mock.assert_called_once_with(start_dt, end_dt)
 
 
 @patch('roboclimate.data_quality.load_csv_files')
@@ -49,4 +75,4 @@ def test_missing_forecasts(dts_mock, load_csv_files_mock):
 
     result = rdq.missing_forecasts(city)
     assert len(result) == 1
-    assert result.iloc[0]['today'] == '2020-12-03'
+    assert result[0] == '2020-12-03'
