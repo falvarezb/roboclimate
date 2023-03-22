7e85079ed520b5ed783de64a5c3f7edfc9a4106a
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-03-22 12:46:52 +0000

    refactoring of join_true_values_and_forecast to improve performance

--- roboclimate/data_analysis.py
+++ roboclimate/data_analysis.py
@@ -1,3 +1,4 @@
+
 import logging
 from math import sqrt
 import pandas as pd
@@ -12,39 +13,42 @@ import roboclimate.util as util
 logger = logging.getLogger(__name__)
 
 
-def load_data(file, weather_variable):
-    return pd.read_csv(file, usecols=[weather_variable, 'dt', 'today'], dtype={'dt': 'int64'})
+def load_data(file):
+    return pd.read_csv(file, dtype={'dt': 'int64'})
+
+# def load_data(file, weather_variable):
+#     return pd.read_csv(file, usecols=[weather_variable, 'dt', 'today'], dtype={'dt': 'int64'})
 
 
-def join_true_temp_and_forecast(true_temp_df, forecast_temp_df, weather_variable):
+def join_true_values_and_forecast(true_values_df, forecast_df):
     """
 
     Joins the records from weather.csv and forecast.csv by the field dt, effectively
-    matching the true temperature with the forecast done over the 5 previous days.
+    matching the true values of the weather variables with the forecast done over the 5 previous days.
     If the forecast of any of the 5 previous days is not available, the entire record is discarded
 
-    true_temp_df
+    true_values_df
     ------------
 
-      temp   dt       today
-    0   0.5  100  2019-11-30
-    1   0.6  200  2019-11-30
+      temp    pressure  .....    dt   today
+    0   0.5   1010               100  2019-11-30
+    1   0.6   999                200  2019-11-30
 
 
-    forecast_temp_df
+    forecast_df
     ----------------
 
-      temp   dt       today
-    0   1.0  100  2019-11-30
-    1   2.0  100  2019-11-28
-    2   1.5  100  2019-11-27
-    3   3.0  100  2019-11-29
-    4   4.0  100  2019-11-26
-    5   5.0  200  2019-11-30
-    6   2.0  200  2019-11-28
-    7   4.0  200  2019-11-27
-    8   3.0  200  2019-11-29
-    9   1.0  200  2019-11-26
+      temp     pressure     ....    dt      today
+    0   1.0    1000                 100     2019-11-29
+    1   2.0    1002                 100     2019-11-27
+    2   1.5    990                  100     2019-11-26
+    3   3.0    1020                 100     2019-11-28
+    4   4.0    992                  100     2019-11-25
+    5   5.0    1013                 200     2019-11-29
+    6   2.0    1012                 200     2019-11-27
+    7   4.0    1000                 200     2019-11-26
+    8   3.0    1030                 200     2019-11-28
+    9   1.0    1000                 200     2019-11-25
 
 
     result
@@ -54,26 +58,65 @@ def join_true_temp_and_forecast(true_temp_df, forecast_temp_df, weather_variable
     0   0.5  100  2019-11-30  4.0  1.5  2.0  3.0  1.0
     1   0.6  200  2019-11-30  1.0  4.0  2.0  3.0  5.0
 
+
+       pressure   dt       today   t5   t4   t3   t2   t1
+    0  1010       100  2019-11-30  992  990  1002 1020 1000
+    1  999        200  2019-11-30  1000 1000 1012 1030 1013
+
+    .....
+
     """
 
     headers = ['t5', 't4', 't3', 't2', 't1']
     # discarding empty values
-    true_temp_df = true_temp_df[true_temp_df[weather_variable] != np.nan]
-    df = pd.DataFrame()
-    for row in true_temp_df.iterrows():
+    true_values_df.dropna(inplace=True)
+    # initializing dataframes
+    dfs_dict = {weather_variable: pd.DataFrame() for _, weather_variable in config.weather_variables.items()}
+
+    for row in true_values_df.iterrows():
         try:
-            tees = forecast_temp_df[forecast_temp_df['dt'] == row[1]['dt']].sort_values('today').T.loc[weather_variable]
-            if len(tees) == len(headers):
-                # df with columns: [t5, t4, t3, t2, t1]
-                tees_df = pd.DataFrame({i: [j] for i, j in zip(headers, tees)}, index=[row[0]])
-                # joining dfs: [temp, dt, today] + [t5, t4, t3, t2, t1]
-                df = df.append(pd.DataFrame([row[1]]).join(tees_df))
+            # 1st row (dt=100)
+            # -----------------
+            #               0           1           2           3           4
+            # temp          4.0         1.5         2.0         3.0         1.0
+            # pressure      991         990         1002        1020        1000
+            # ....
+            # dt            100         100         100         100         100
+            # today         2019-11-25  2019-11-26  2019-11-27  2019-11-28  2019-11-29
+            #
+            transposed_forecast_df = forecast_df[forecast_df['dt'] == row[1]['dt']].sort_values('today').T
+            if transposed_forecast_df.shape[1] == len(headers):
+                for weather_variable, df in dfs_dict.items():
+
+                    # weather_variable = temp
+                    # ------------------------
+                    #
+                    #       t5  t4  t3  t2  t1
+                    # 0     4.0 1.5 2.0 3.0 1.0
+                    #
+                    tees_df = pd.DataFrame({i: [j] for i, j in zip(headers, transposed_forecast_df.loc[weather_variable])}, index=[row[0]])
+                    
+                    # weather_variable = temp
+                    # ------------------------
+                    #     temp  dt   today
+                    # 0   0.5   100  2019-11-30
+                    # 
+                    weather_variable_df = pd.DataFrame({weather_variable: [row[1][weather_variable]], 'dt': [
+                                                       row[1]['dt']], 'today': [row[1]['today']]}, index=[row[0]])
+                    
+                    # weather_variable = temp
+                    # ------------------------
+                    #     temp  dt   today      t5  t4  t3  t2  t1
+                    # 0   0.5   100  2019-11-30 4.0 1.5 2.0 3.0 1.0                    
+                    # 
+                    joined_df = df.append(weather_variable_df.join(tees_df))
+                    dfs_dict[weather_variable] = joined_df
             # else:
             #     logger.warning(f"number of {weather_variable} {len(temps)} != 5 for timestamp {row[1]['dt']}")
         except Exception:
             logger.error(f"Error while processing row \n {row[1]}", exc_info=True)
 
-    return df
+    return dfs_dict
 
 
 def forecast_precision(joined_data, weather_variable):
@@ -86,7 +129,7 @@ def forecast_precision(joined_data, weather_variable):
     }
 
 
-def analyse_data(weather_variable: str):
+def analyse_data():
     # london_df = read_historical_data("london_weather_historical_data.csv")
 
     for city_name in config.cities.keys():
@@ -94,22 +137,23 @@ def analyse_data(weather_variable: str):
             # file pointers
             weather_file = util.csv_file_path(config.csv_folder, config.weather_resources[0], city_name)
             forecast_file = util.csv_file_path(config.csv_folder, config.weather_resources[1], city_name)
-            join_file = util.csv_file_path(config.csv_folder, "join", city_name, weather_variable)
-            metrics_file = util.csv_file_path(config.csv_folder, "metrics", city_name, weather_variable)
-            
-            join_data_df = join_true_temp_and_forecast(load_data(weather_file, weather_variable), load_data(forecast_file, weather_variable), weather_variable)
-            join_data_df.to_csv(join_file, index=False)
-            # metrics = forecast_precision(join_data_df, london_df)
-            metrics = forecast_precision(join_data_df, weather_variable)
-            pd.DataFrame(metrics).to_csv(metrics_file, index=False)
+            join_data_dict = join_true_values_and_forecast(load_data(weather_file), load_data(forecast_file))
+
+            for _, weather_variable in config.weather_variables.items():
+                join_file = util.csv_file_path(config.csv_folder, "join", city_name, weather_variable)
+                metrics_file = util.csv_file_path(config.csv_folder, "metrics", city_name, weather_variable)
+                join_data_dict[weather_variable].to_csv(join_file, index=False)
+                # metrics = forecast_precision(join_data_df, london_df)
+                metrics = forecast_precision(join_data_dict[weather_variable], weather_variable)
+                pd.DataFrame(metrics).to_csv(metrics_file, index=False)
         except Exception:
             logger.error(f"Error while processing {weather_variable} for {city_name}", exc_info=True)
 
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level='INFO')
-    for _, weather_variable in config.weather_variables.items():
-        analyse_data(weather_variable)
+    # for _, weather_variable in config.weather_variables.items():
+    analyse_data()
     logger.info('END')
 
 
--- tests/data_analysis_test.py
+++ tests/data_analysis_test.py
@@ -1,19 +1,7 @@
 import pandas as pd
-from roboclimate.data_analysis import load_data, join_true_temp_and_forecast
+from roboclimate.data_analysis import load_data, join_true_values_and_forecast
 import roboclimate.data_analysis as rda
 import numpy as np
-
-
-def test_load_true_temp():
-    data = load_data('tests/csv_files/weather.csv', 'temp')
-    expected_df = pd.DataFrame({'temp': [10, 20], 'dt': [100, 200], 'today': ['2019-11-26', '2019-11-27']})
-    assert data.equals(expected_df)
-
-
-def test_load_forecast_temp():
-    data = load_data('tests/csv_files/forecast.csv', 'temp')
-    expected_df = pd.DataFrame({'temp': [10, 20], 'dt': [100, 200], 'today': ['2019-11-26', '2019-11-27']})
-    assert data.equals(expected_df)
 
 
 def test_forecast_precision():
@@ -36,23 +24,24 @@ def test_forecast_precision():
 
 def test_join_one_element_in_current_weather():
     """
-        temp   dt       today
-    0   0.5    100      2019-11-30
+        temp   pressure     dt       today
+    0   0.5    1020         100      2019-11-30
 
 
-        temp   dt       today
-    0   1      100      2019-11-30
-    1   2      100      2019-11-28
-    2   1.5    100      2019-11-27
-    3   3      100      2019-11-29
-    4   4      100      2019-11-26
+        temp   pressure     dt       today
+    0   1      1000         100      2019-11-29
+    1   2      990          100      2019-11-27
+    2   1.5    1010         100      2019-11-26
+    3   3      1000         100      2019-11-28
+    4   4      1020         100      2019-11-25
 
     """
-    current_weather_df = pd.DataFrame({'temp': [0.5], 'dt': [100], 'today': ['2019-11-30']})
-    forecast_df = pd.DataFrame({'temp': [1, 2, 1.5, 3, 4], 'dt': [100] * 5, 'today': ['2019-11-30', '2019-11-28', '2019-11-27', '2019-11-29', '2019-11-26']})
+    current_weather_df = pd.DataFrame({'temp': [0.5], 'pressure': [1020], 'dt': [100], 'today': ['2019-11-30']})
+    forecast_df = pd.DataFrame({'temp': [1, 2, 1.5, 3, 4], 'pressure': [1000,990,1010,1000,1020], 'dt': [100] * 5, 'today': ['2019-11-29', '2019-11-27', '2019-11-26', '2019-11-28', '2019-11-25']})
 
-    result = join_true_temp_and_forecast(current_weather_df, forecast_df, 'temp')
-    assert result.equals(pd.DataFrame({'temp': [0.5], 'dt': [100], 'today': ['2019-11-30'], 't5': [4.0], 't4': [1.5], 't3': [2.0], 't2': [3.0], 't1': [1.0]}))
+    result = join_true_values_and_forecast(current_weather_df, forecast_df)
+    assert result['temp'].equals(pd.DataFrame({'temp': [0.5], 'dt': [100], 'today': ['2019-11-30'], 't5': [4.0], 't4': [1.5], 't3': [2.0], 't2': [3.0], 't1': [1.0]}))
+    assert result['pressure'].equals(pd.DataFrame({'pressure': [1020], 'dt': [100], 'today': ['2019-11-30'], 't5': [1020], 't4': [1010], 't3': [990], 't2': [1000], 't1': [1000]}))
 
 
 def test_join_two_elements_in_current_weather():
@@ -78,11 +67,11 @@ def test_join_two_elements_in_current_weather():
     forecast_df = pd.DataFrame({'temp': [1, 2, 1.5, 3, 4, 5, 2, 4, 3, 1], 'dt': [100] * 5 + [200] * 5,
                                 'today': ['2019-11-30', '2019-11-28', '2019-11-27', '2019-11-29', '2019-11-26'] * 2})
 
-    result = join_true_temp_and_forecast(current_weather_df, forecast_df, 'temp')
+    result = join_true_values_and_forecast(current_weather_df, forecast_df)
     expected = pd.DataFrame({'temp': [0.5, 0.6], 'dt': [100, 200], 'today': ['2019-11-30'] * 2,
                              't5': [4.0, 1], 't4': [1.5, 4], 't3': [2.0, 2], 't2': [3.0, 3], 't1': [1.0, 5]})
 
-    assert result.equals(expected)
+    assert result['temp'].equals(expected)
 
 
 def test_join_record_discarded_when_missing_forecasts():
@@ -100,8 +89,8 @@ def test_join_record_discarded_when_missing_forecasts():
     current_weather_df = pd.DataFrame({'temp': [0.5], 'dt': [100], 'today': ['2019-11-30']})
     forecast_df = pd.DataFrame({'temp': [1, 2, 1.5, 3], 'dt': [100] * 4, 'today': ['2019-11-30', '2019-11-28', '2019-11-29', '2019-11-26']})
 
-    result = join_true_temp_and_forecast(current_weather_df, forecast_df, 'temp')
-    assert result.equals(pd.DataFrame())
+    result = join_true_values_and_forecast(current_weather_df, forecast_df)
+    assert result['temp'].equals(pd.DataFrame())
 
 
 def test_join_record_discarded_when_has_empty_value():
@@ -120,5 +109,5 @@ def test_join_record_discarded_when_has_empty_value():
     current_weather_df = pd.DataFrame({'temp': [np.nan], 'dt': [100], 'today': ['2019-11-30']})
     forecast_df = pd.DataFrame({'temp': [1, 2, 1.5, 3], 'dt': [100] * 4, 'today': ['2019-11-30', '2019-11-28', '2019-11-29', '2019-11-26']})
 
-    result = join_true_temp_and_forecast(current_weather_df, forecast_df, 'temp')
-    assert result.equals(pd.DataFrame())
+    result = join_true_values_and_forecast(current_weather_df, forecast_df)
+    assert result['temp'].equals(pd.DataFrame())
