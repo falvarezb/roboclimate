1d495248649f1d24761cbc9f99e237de199b67f3
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-07-26 16:20:06 +0100

    terraform

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -9,14 +9,31 @@ provider "aws" {
 
 }
 
+locals {
+  weather = {
+    pkg_folder = "weather_pkg"
+    artifact = "weather_pkg.zip"
+    function_name = "t_roboclimate_weather"    
+  }
+  lambda_iam_role = "t_roboclimate_weather_role"
+  lambda_policy = "AWSLambdaExecute"
+
+}
+
+data "archive_file" "weather" {
+  type = "zip"
+  source_dir  = "${path.module}/${local.weather.pkg_folder}"
+  output_path = "${path.module}/${local.weather.artifact}"
+}
+
 resource "aws_lambda_function" "weather" {
-  function_name = "t_roboclimate_weather"
+  function_name = local.weather.function_name
   runtime = "python3.8"
   handler = "weather_spider.weather_handler"
   role = aws_iam_role.lambda_exec.arn
-  filename = "${path.module}/roboclimate-weather-package.zip"
+  filename = "${path.module}/${local.weather.artifact}"
   timeout = 10
-  source_code_hash = "3"
+  source_code_hash = data.archive_file.weather.output_base64sha256
   publish = true
 
   environment {
@@ -34,7 +51,7 @@ resource "aws_cloudwatch_log_group" "weather" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "t_roboclimate_weather_role"
+  name = local.lambda_iam_role
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -52,6 +69,6 @@ resource "aws_iam_role" "lambda_exec" {
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
+  policy_arn = "arn:aws:iam::aws:policy/${local.lambda_policy}"
 }
 
