386b40900a0dfff359bd6251c75e62975ee1243c
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-07-26 16:55:56 +0100

    manual deploy

--- deploy/manual/artifact.sh
+++ deploy/manual/artifact.sh
@@ -14,9 +14,9 @@ source ./"$CONFIG_NAME".sh
 THIS_FOLDER=$(PWD)
 
 mkdir pkg
-pip install --target pkg -r requirements.txt
+pip install --target pkg -r "$ROBOCLIMATE_HOME"/lambda_requirements.txt
 cd pkg || exit
 zip -r ../"$ARTIFACT_NAME" .
-cd ../../roboclimate || exit
+cd "$ROBOCLIMATE_HOME"/roboclimate || exit
 zip -g "$THIS_FOLDER/$ARTIFACT_NAME" $PYTHON_FILES
 rm -rf "$THIS_FOLDER/pkg"
