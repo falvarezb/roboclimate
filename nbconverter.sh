57a312147f59ca49054ac17b7df2bd8f919cc12f
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-07-28 23:15:09 +0100

    cleanup

--- nbconverter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# jupyter nbconvert --to html --template hidecode my_notebook.ipynb
-
-jupyter nbconvert --ExecutePreprocessor.kernel_name=python3  --no-input --to html --execute ./roboclimate.ipynb
-cp ./roboclimate.html /usr/share/nginx/html/roboclimate/index.html
