0487404624e67f273f1d72fc7a631a08ef9a07f9
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-07-28 23:21:03 +0100

    refactoring

--- deploy/terraform/artifact_prep.sh
+++ deploy/terraform/artifact_prep.sh
@@ -4,16 +4,16 @@ set -xv
 
 function prepare_pkg() {
     lambda_function="$1"    
-    if [ $lambda_function != "weather" -a $lambda_function != "forecast"]; then
+    if [ "$lambda_function" != "weather" ] && [ "$lambda_function" != "forecast" ]; then
         echo "resource must be one of 'weather' or 'forecast'"
         exit 1
     fi
 
     pkg_folder=${lambda_function}_pkg
-    rm -rf $pkg_folder
-    mkdir $pkg_folder
-    pip install --target $pkg_folder -r $ROBOCLIMATE_HOME/lambda_requirements.txt
-    cp $ROBOCLIMATE_HOME/roboclimate/${lambda_function}_spider.py $ROBOCLIMATE_HOME/roboclimate/common.py $pkg_folder
+    rm -rf "$pkg_folder"
+    mkdir "$pkg_folder"
+    pip install --target "$pkg_folder" -r "$ROBOCLIMATE_HOME"/lambda_requirements.txt
+    cp "$ROBOCLIMATE_HOME"/roboclimate/"${lambda_function}"_spider.py "$ROBOCLIMATE_HOME"/roboclimate/common.py "$pkg_folder"
 }
 
 prepare_pkg "weather"
