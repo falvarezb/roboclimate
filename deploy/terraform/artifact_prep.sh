a5781704c75c8ae68a5ad3a2cd7e247e128b780b
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-07-29 13:56:09 +0100

    sh refactoring

--- deploy/terraform/artifact_prep.sh
+++ deploy/terraform/artifact_prep.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -xv
+# https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425?permalink_comment_id=3945021
+set -exuvo pipefail
 
 function prepare_pkg() {
     lambda_function="$1"    
