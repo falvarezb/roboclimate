0745128317e92e001e2a049c23af010e4f187174
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-08-07 20:16:46 +0100

    artifact prep refactoring

--- deploy/terraform/artifact_prep.sh
+++ deploy/terraform/artifact_prep.sh
@@ -3,18 +3,20 @@
 # https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425?permalink_comment_id=3945021
 set -exuvo pipefail
 
-function prepare_pkg() {
-    lambda_function="$1"    
-    if [ "$lambda_function" != "weather" ] && [ "$lambda_function" != "forecast" ]; then
-        echo "resource must be one of 'weather' or 'forecast'"
-        exit 1
-    fi
+if [ $# -lt 1 ]; then
+    echo "USAGE ./artifact_prep.sh <weather|forecast>"
+    exit 1
+fi
 
-    pkg_folder=${lambda_function}_pkg
-    rm -rf "$pkg_folder"
-    mkdir "$pkg_folder"
-    pip install --target "$pkg_folder" -r "$ROBOCLIMATE_HOME"/lambda_requirements.txt
-    cp "$ROBOCLIMATE_HOME"/roboclimate/"${lambda_function}"_spider.py "$ROBOCLIMATE_HOME"/roboclimate/common.py "$pkg_folder"
-}
 
-prepare_pkg "weather"
+lambda_function="$1"    
+if [ "$lambda_function" != "weather" ] && [ "$lambda_function" != "forecast" ]; then
+    echo "USAGE ./artifact_prep.sh <weather|forecast>"
+    exit 1
+fi
+
+pkg_folder=${lambda_function}_pkg
+rm -rf "$pkg_folder"
+mkdir "$pkg_folder"
+pip install --target "$pkg_folder" -r "$ROBOCLIMATE_HOME"/lambda_requirements.txt
+cp "$ROBOCLIMATE_HOME"/roboclimate/"${lambda_function}"_spider.py "$ROBOCLIMATE_HOME"/roboclimate/common.py "$pkg_folder"
