521fc883bb2e7439ec6e7903e03c42a48e6733a6
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-08-17 18:02:27 +0100

    refactoring

--- terraform/connefs.sh
+++ terraform/connefs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 NAT_INSTANCE_IP=$(terraform output -raw nat_public_ip)
 EFS_INSTANCE_IP=$(terraform output -raw efs_instance_private_ip)
 ssh -i ~/.ssh/fjab-aws.pem -A -o StrictHostKeyChecking=no -J ec2-user@"$NAT_INSTANCE_IP" ubuntu@"$EFS_INSTANCE_IP"
