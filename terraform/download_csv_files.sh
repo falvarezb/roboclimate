c4a8eab2e784b05c7ce2da90e2aed9c12a7fb95a
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-08-26 19:35:27 +0100

    updating shell scripts to handle csv files

--- terraform/download_csv_files.sh
+++ terraform/download_csv_files.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+
+if [ $# -gt 0 ]; then
+    CSV_FOLDER="$1"
+else
+    CSV_FOLDER="$ROBOCLIMATE_CSV_FILES_PATH"
+fi
+
 NAT_INSTANCE_IP=$(terraform output -raw nat_public_ip)
 EFS_INSTANCE_IP=$(terraform output -raw efs_instance_private_ip)
-scp -i ~/.ssh/fjab-aws.pem -A -o StrictHostKeyChecking=no -J ec2-user@"$NAT_INSTANCE_IP" ubuntu@"$EFS_INSTANCE_IP":/home/ubuntu/efs/lwf/*.csv "$ROBOCLIMATE_CSV_FILES_PATH"
+scp -i ~/.ssh/fjab-aws.pem -A -o StrictHostKeyChecking=no -J ec2-user@"$NAT_INSTANCE_IP" ubuntu@"$EFS_INSTANCE_IP":/home/ubuntu/efs/lwf/*.csv "$CSV_FOLDER"
--- terraform/upload_csv_files.sh
+++ terraform/upload_csv_files.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+
+if [ $# -gt 0 ]; then
+    CSV_FOLDER="$1"
+else
+    CSV_FOLDER="$ROBOCLIMATE_CSV_FILES_PATH"
+fi
+
 NAT_INSTANCE_IP=$(terraform output -raw nat_public_ip)
 EFS_INSTANCE_IP=$(terraform output -raw efs_instance_private_ip)
-scp -i ~/.ssh/fjab-aws.pem -A -o StrictHostKeyChecking=no -J ec2-user@"$NAT_INSTANCE_IP" "$ROBOCLIMATE_CSV_FILES_PATH"/weather*.csv "$ROBOCLIMATE_CSV_FILES_PATH"/forecast*.csv ubuntu@"$EFS_INSTANCE_IP":/home/ubuntu/efs/lwf
+scp -i ~/.ssh/fjab-aws.pem -A -o StrictHostKeyChecking=no -J ec2-user@"$NAT_INSTANCE_IP" "$CSV_FOLDER"/weather*.csv "$CSV_FOLDER"/forecast*.csv ubuntu@"$EFS_INSTANCE_IP":/home/ubuntu/efs/lwf
