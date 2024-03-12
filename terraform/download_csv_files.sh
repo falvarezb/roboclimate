31b3259bccf2771472f10531faa6995da7906a85
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2024-03-11 22:13:07 -0300

    download csv files script update

--- terraform/download_csv_files.sh
+++ terraform/download_csv_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 if [ $# -gt 0 ]; then
     CSV_FOLDER="$1"
 else
@@ -11,7 +13,8 @@ EFS_INSTANCE_IP=$(terraform output -raw efs_instance_private_ip)
 scp -i ~/.ssh/fjab-aws.pem -A -p -o StrictHostKeyChecking=no -J ec2-user@"$NAT_INSTANCE_IP" ubuntu@"$EFS_INSTANCE_IP":/home/ubuntu/efs/lwf/*.csv "$CSV_FOLDER"
 rc=$?
 if [[ $rc != 0 ]]; then 
-    echo "ERROR: make sure 'my_ip' is up to date and private key has been added to ssh-agent"    
+    echo "ERROR: make sure 'my_ip' is up to date and private key has been added to ssh-agent"  
+    echo "The list private keys added to ssh-agent can be viewed with 'ssh-add -l' and added with 'ssh-add ~/.ssh/my-key.pem'"  
 fi
 exit $rc
 
