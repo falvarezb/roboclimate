660f11fd78df8092b91e2a85405e2120f0761b3c
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2024-05-21 12:34:26 -0300

    added comments to scripts to connect to EFS instance

--- terraform/connefs.sh
+++ terraform/connefs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+
 NAT_INSTANCE_IP=$(terraform output -raw nat_public_ip)
 EFS_INSTANCE_IP=$(terraform output -raw efs_instance_private_ip)
 ssh -i ~/.ssh/fjab-aws.pem -A -o StrictHostKeyChecking=no -J ec2-user@"$NAT_INSTANCE_IP" ubuntu@"$EFS_INSTANCE_IP"
@@ -7,7 +7,7 @@ ssh -i ~/.ssh/fjab-aws.pem -A -o StrictHostKeyChecking=no -J ec2-user@"$NAT_INST
 # DO NOT do set -e, as we want to check the return code of the scp command
 rc=$?
 if [[ $rc != 0 ]]; then 
-    echo "ERROR: make sure 'my_ip' is up to date and private key has been added to ssh-agent"    
+    echo "ERROR: make sure 'my_ip' is up to date, the private key has been added to ssh-agent and the EC2's EFS instance is up and running"    
     echo "The list private keys added to ssh-agent can be viewed with 'ssh-add -l' and added with 'ssh-add ~/.ssh/my-key.pem'"  
 fi
 exit $rc
--- terraform/download_csv_files.sh
+++ terraform/download_csv_files.sh
@@ -13,8 +13,7 @@ scp -i ~/.ssh/fjab-aws.pem -A -p -o StrictHostKeyChecking=no -J ec2-user@"$NAT_I
 # DO NOT do set -e, as we want to check the return code of the scp command
 rc=$?
 if [[ $rc != 0 ]]; then 
-    echo "ERROR: make sure 'my_ip' is up to date and private key has been added to ssh-agent"  
+    echo "ERROR: make sure 'my_ip' is up to date, the private key has been added to ssh-agent and the EC2's EFS instance is up and running"
     echo "The list private keys added to ssh-agent can be viewed with 'ssh-add -l' and added with 'ssh-add ~/.ssh/my-key.pem'"  
 fi
 exit $rc
-
